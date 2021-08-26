0650e90134f402099c7cf4eda03d2b785863865d
quocsu3004 <huynhquocsu96@gmail.com>
2021-08-26 15:11:33 +0700

    Almost Done(Not Test)

---
+++ Device Check App/Resources/Database/ListViewAdapter.cs
@@ -0,0 +1,63 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Device_Check_App.Resources.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Device_Check_App.Resources.Database
+{
+    public class ViewHolder : Java.Lang.Object
+    {
+        public TextView txtView_DeviceName { get; set; }
+        public TextView txtView_Status { get; set; }
+        public TextView txtView_Borrower { get; set; }
+        public TextView txtView_BorrowerTeam { get; set; }
+    }
+
+    public class ListViewAdapter : BaseAdapter
+    {
+        private Activity activity;
+        private List<Device> listDevice;
+        public ListViewAdapter(Activity activity, List<Device> listDevice)
+        {
+            this.activity = activity;
+            this.listDevice = listDevice;
+        }
+
+        public override int Count
+        {
+            get { return listDevice.Count; }
+        }
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return listDevice[position].Id;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView ?? activity.LayoutInflater.Inflate(Resource.Layout.ListView, parent, false);
+            var txtDeviceName = view.FindViewById<TextView>(Resource.Id.txtView_DeviceName);
+            var txtStatus = view.FindViewById<TextView>(Resource.Id.txtView_Status);
+            var txtBorrower = view.FindViewById<TextView>(Resource.Id.txtView_Borrower);
+            var txtBorrowerTeam = view.FindViewById<TextView>(Resource.Id.txtView_BorrowerTeam);
+            txtDeviceName.Text = listDevice[position].Device_Name;
+            txtStatus.Text = listDevice[position].Status;
+            txtBorrower.Text = listDevice[position].Borrower;
+            txtBorrowerTeam.Text = listDevice[position].Team_Borrower;
+            return view;
+
+        }
+    }
+
+}
---
+++ Device Check App/Resources/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Device_Check_App.Resources.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public string Id { get; set; }
+        [Indexed]
+        public string DeviceId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
